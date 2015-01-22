f946c598d7b1bd7fc4221f46f122daf51175818b
leemendoza <leemendoza@gmail.com>
2015-01-22 07:20:50 -0600

    downloading items and converting to a dataframe works

--- wells.R
+++ wells.R
@@ -1,25 +1,19 @@
-## replication - the ultimate standard for scientific evidence
-## reproducible research: make analytic data and code available
-## so that other may reproduce findings
-
-## can at least redo analysis
-
-## research pipeline
-## measured data - analytic data - computational results - graphs, tables, etc
-
-## what do we need?
-## analytic data are available (not raw data)
-## analytic code
-## Documenation
-## Standardization
-
-interview = c(27, 28, 27, 23, 26)
-exercise = c(9,9,7,8,7.5)
-plot(interview, exercise, xlim = c(15, 30), ylim = c(5, 10))
 
 library(XML)
+
+## this is the location of the Wells tracking data maintained by the regions
+## and NFA
 URL <- "http://dhhnet/departments/oaas/PO/_vti_bin/ListData.svc/WellsTracking"
-dat <- readLines(URL)
+dat <- xmlParse(readLines(URL))
 
+## get the individual list items
+items = getNodeSet(dat, "//m:properties")
 
+## convert to a data frame
+df = xmlToDataFrame(items)
+
+## clean up date data
+df$DateLetterSent = as.Date(df$DateLetterSent)
+df$Modified = as.Date(df$Modified)
+df$Created = as.Date(df$Created)
 
